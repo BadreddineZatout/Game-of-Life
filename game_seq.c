f1c47af6cc1dd7b0601365b8bff47e1b03ba0925
BadreddineZatout <hb_zatout@esi.dz>
2021-06-01 23:10:40 +0100

    code sequenciel de game of life

---
+++ game_seq.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+/*  game of lide    */
+
+
+#define  ml 80
+#define mc 100
+
+enum{
+    dead, alive
+};
+
+void print_matrix(char *m, int l, int c){
+    for(int i=0; i<c+2; i++){
+        printf("--");
+    }
+    printf("\n");
+    for(int i=0; i<ml; i++){
+        printf("| ");
+        for(int j=0; j<c; j++){
+            if(*(m + i*c+ j)){
+                printf("##");
+            }else{
+                printf("  ");
+            }
+        }
+        printf(" |\n");
+    }
+    for(int i=0; i<c+2; i++){
+        printf("--");
+    }
+}
+
+char update_cell(char *m, int ipos, int jpos, int l, int c){
+    int nb =0;
+    int i, j;
+    for (i = ipos-1; i <= ipos+1; i++)
+    {
+        if(i>=0 && i<l){
+            for(j=jpos-1; j<=jpos+1; j++){
+                if(j>=0 && j<c){
+                    if(i!=ipos || j!=jpos){
+                        if(*(m + i * c + j)==alive){
+                            nb++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if(*(m + ipos * c + jpos)==alive){
+        if(nb==2 || nb==3) return alive;
+        else return dead;
+    }else{
+        if(nb==3) return alive;
+        else return dead;
+    }
+}
+
+void updateV1(char *m, char *bis, int l, int c){
+    for(int i=0; i<l; i++){
+        for(int j=0; j<c; j++){
+            *(bis + i * c +j) = update_cell(m,i,j,l,c);
+        }
+    }
+    for (int i = 0; i < l; i++)
+    {
+        for(int j=0; j<c; j++){
+            *(m + i * c +j) = *(bis + i * c +j);
+        }
+    }
+    
+}
+
+void set_matrix(char *m, int l, int c,char *s, int sl, int sc, int x, int y){
+    for (int i = y; i < y+sl; i++)
+    {
+        for (int j = x; j < x+sc; j++)
+        {
+            *(m + i * c + j) = *(s + (i-y) * sc + (j-x));
+        }
+        
+    }
+    
+}
+
+int main(void){
+    char matrix[ml][mc] = {dead};
+    char bis[ml][mc]= {dead};
+    char canon[9][36] = {
+          {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+          {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0},
+          {0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+          {0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+          {1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+          {1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0},
+          {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+          {0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+          {0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        };
+    set_matrix(&matrix[0][0], ml, mc, &canon[0][0], 9, 36, 10, 10);
+    while(1){
+        system("cls");
+        updateV1(&matrix[0][0], &bis[0][0], ml, mc);
+        print_matrix(&matrix[0][0], ml, mc);
+        Sleep(10);
+    }
+    return 0;
+}
